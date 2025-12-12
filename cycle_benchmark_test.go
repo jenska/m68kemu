020edcdb6b55ae6b23e0d91f0bd62db36353cb55
Jens Kaiser <jens.kaiser.de@gmail.com>
2025-12-12 19:43:23 +0100

    stabelized benchmark

--- cycle_benchmark_test.go
+++ cycle_benchmark_test.go
@@ -2,27 +2,25 @@ package m68kemu
 
 import "testing"
 
+func prepareEnvironment(b *testing.B) CPU {
+	b.Helper()
+	cpu, ram := newEnvironment(b)
+	code := assemble(b, "loop: ADDQ.L #1, D0\nMOVE.L D0, D1\nBRA.S loop")
+	for offset, value := range code {
+		addr := cpu.regs.PC + uint32(offset)
+		if err := ram.Write(Byte, addr, uint32(value)); err != nil {
+			b.Fatalf("failed to seed program byte at %04x: %v", addr, err)
+		}
+	}
+	return cpu
+}
+
 func BenchmarkRunEightMillionCycles(b *testing.B) {
 	const cycleBudget = 8_000_000
-
-	code := assemble(b, "loop: NOP\nBRA loop")
-
-	b.ReportAllocs()
-
+	cpu := prepareEnvironment(b)
 	for i := 0; i < b.N; i++ {
-		cpu, ram := newEnvironment(b)
-
-		for offset, value := range code {
-			addr := cpu.regs.PC + uint32(offset)
-			if err := ram.Write(Byte, addr, uint32(value)); err != nil {
-				b.Fatalf("failed to seed program byte at %04x: %v", addr, err)
-			}
-		}
-
-		b.ResetTimer()
 		if err := cpu.RunCycles(cycleBudget); err != nil {
 			b.Fatalf("RunCycles failed: %v", err)
 		}
-		b.StopTimer()
 	}
 }
