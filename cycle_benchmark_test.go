3fed6808eef4f31ff2c921b4fd902af4a9b4b8fc
Jens Kaiser <jens.kaiser.de@gmail.com>
2025-12-12 19:46:06 +0100

    modernized benchmark

--- cycle_benchmark_test.go
+++ cycle_benchmark_test.go
@@ -2,8 +2,8 @@ package m68kemu
 
 import "testing"
 
-func prepareEnvironment(b *testing.B) CPU {
-	b.Helper()
+func BenchmarkRunEightMillionCycles(b *testing.B) {
+	const cycleBudget = 8_000_000
 	cpu, ram := newEnvironment(b)
 	code := assemble(b, "loop: ADDQ.L #1, D0\nMOVE.L D0, D1\nBRA.S loop")
 	for offset, value := range code {
@@ -12,13 +12,8 @@ func prepareEnvironment(b *testing.B) CPU {
 			b.Fatalf("failed to seed program byte at %04x: %v", addr, err)
 		}
 	}
-	return cpu
-}
 
-func BenchmarkRunEightMillionCycles(b *testing.B) {
-	const cycleBudget = 8_000_000
-	cpu := prepareEnvironment(b)
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		if err := cpu.RunCycles(cycleBudget); err != nil {
 			b.Fatalf("RunCycles failed: %v", err)
 		}
