b33bf879a04a1e9f009a5f3087697c46a58e8273
Jens <jenska@users.noreply.github.com>
2025-12-14 10:55:04 +0100

    Implement status register control instructions

--- status_control.go
+++ status_control.go
@@ -4,6 +4,27 @@ func init() {
 	registerInstruction(trapv, 0x4e76, 0xffff, 0, constantCycles(4))
 	registerInstruction(resetInstruction, 0x4e70, 0xffff, 0, constantCycles(132))
 	registerInstruction(stop, 0x4e72, 0xffff, 0, constantCycles(4))
+
+	registerInstruction(oriToCcr, 0x003c, 0xffff, 0, constantCycles(20))
+	registerInstruction(oriToSr, 0x007c, 0xffff, 0, constantCycles(20))
+	registerInstruction(andiToCcr, 0x023c, 0xffff, 0, constantCycles(20))
+	registerInstruction(andiToSr, 0x027c, 0xffff, 0, constantCycles(20))
+	registerInstruction(eoriToCcr, 0x0a3c, 0xffff, 0, constantCycles(20))
+	registerInstruction(eoriToSr, 0x0a7c, 0xffff, 0, constantCycles(20))
+
+	const controlSourceMask = eaMaskDataRegister | eaMaskIndirect | eaMaskPostIncrement |
+		eaMaskPreDecrement | eaMaskDisplacement | eaMaskIndex |
+		eaMaskAbsoluteShort | eaMaskAbsoluteLong | eaMaskImmediate |
+		eaMaskPCDisplacement | eaMaskPCIndex
+	const controlDestinationMask = eaMaskDataRegister | eaMaskIndirect | eaMaskPostIncrement |
+		eaMaskPreDecrement | eaMaskDisplacement | eaMaskIndex |
+		eaMaskAbsoluteShort | eaMaskAbsoluteLong
+
+	registerInstruction(moveFromSr, 0x40c0, 0xffc0, controlDestinationMask, moveControlCycleCalculator(Word))
+	registerInstruction(moveToCcr, 0x44c0, 0xffc0, controlSourceMask, moveControlCycleCalculator(Byte))
+	registerInstruction(moveToSr, 0x46c0, 0xffc0, controlSourceMask, moveControlCycleCalculator(Word))
+
+	registerInstruction(rte, 0x4e73, 0xffff, 0, constantCycles(20))
 }
 
 func trapv(cpu *cpu) error {
@@ -57,4 +78,100 @@ func clrTstCycleCalculator() cycleCalculator {
 		reg := opcode & 0x7
 		return 4 + eaAccessCycles(mode, reg, size)
 	}
+}
+
+func logicalCcrOp(cpu *cpu, op func(uint16, uint16) uint16) error {
+	imm, err := cpu.popPc(Word)
+	if err != nil {
+		return err
+	}
+	current := cpu.regs.SR
+	newCcr := op(current&0xff, uint16(imm)) & 0xff
+	cpu.setSR((current & 0xff00) | newCcr)
+	return nil
+}
+
+func logicalSrOp(cpu *cpu, op func(uint16, uint16) uint16) error {
+	if cpu.regs.SR&srSupervisor == 0 {
+		return cpu.exception(XPrivViolation)
+	}
+	imm, err := cpu.popPc(Word)
+	if err != nil {
+		return err
+	}
+	cpu.setSR(op(cpu.regs.SR, uint16(imm)))
+	return nil
+}
+
+func oriToCcr(cpu *cpu) error  { return logicalCcrOp(cpu, func(a, b uint16) uint16 { return a | b }) }
+func oriToSr(cpu *cpu) error   { return logicalSrOp(cpu, func(a, b uint16) uint16 { return a | b }) }
+func andiToCcr(cpu *cpu) error { return logicalCcrOp(cpu, func(a, b uint16) uint16 { return a & b }) }
+func andiToSr(cpu *cpu) error  { return logicalSrOp(cpu, func(a, b uint16) uint16 { return a & b }) }
+func eoriToCcr(cpu *cpu) error { return logicalCcrOp(cpu, func(a, b uint16) uint16 { return a ^ b }) }
+func eoriToSr(cpu *cpu) error  { return logicalSrOp(cpu, func(a, b uint16) uint16 { return a ^ b }) }
+
+func moveControlCycleCalculator(size Size) cycleCalculator {
+	return func(opcode uint16) uint32 {
+		mode := (opcode >> 3) & 0x7
+		reg := opcode & 0x7
+		return 12 + eaAccessCycles(mode, reg, size)
+	}
+}
+
+func moveFromSr(cpu *cpu) error {
+	dst, err := cpu.ResolveSrcEA2(Word)
+	if err != nil {
+		return err
+	}
+	return dst.write(uint32(cpu.regs.SR))
+}
+
+func moveToCcr(cpu *cpu) error {
+	src, err := cpu.ResolveSrcEA(Byte)
+	if err != nil {
+		return err
+	}
+	value, err := src.read()
+	if err != nil {
+		return err
+	}
+	current := cpu.regs.SR & 0xff00
+	cpu.setSR(current | uint16(value&0xff))
+	return nil
+}
+
+func moveToSr(cpu *cpu) error {
+	if cpu.regs.SR&srSupervisor == 0 {
+		return cpu.exception(XPrivViolation)
+	}
+	src, err := cpu.ResolveSrcEA(Word)
+	if err != nil {
+		return err
+	}
+	value, err := src.read()
+	if err != nil {
+		return err
+	}
+	cpu.setSR(uint16(value))
+	return nil
+}
+
+func rte(cpu *cpu) error {
+	if cpu.regs.SR&srSupervisor == 0 {
+		return cpu.exception(XPrivViolation)
+	}
+	newSR, err := cpu.pop(Word)
+	if err != nil {
+		return err
+	}
+	pc, err := cpu.pop(Long)
+	if err != nil {
+		return err
+	}
+	if _, err := cpu.pop(Word); err != nil {
+		return err
+	}
+	cpu.setSR(uint16(newSR))
+	cpu.regs.PC = pc
+	return nil
 }
---
+++ status_control_priv_test.go
@@ -0,0 +1,137 @@
+package m68kemu
+
+import "testing"
+
+func TestLogicalImmediateCcrAndSr(t *testing.T) {
+	cpu, ram := newEnvironment(t)
+	cpu.regs.SR = 0x0000
+
+	addr := cpu.regs.PC
+	ram.Write(Word, addr, 0x003c)
+	ram.Write(Word, addr+uint32(Word), 0x0012)
+	ram.Write(Word, addr+uint32(2*Word), 0x023c)
+	ram.Write(Word, addr+uint32(3*Word), 0x007f)
+	ram.Write(Word, addr+uint32(4*Word), 0x0a3c)
+	ram.Write(Word, addr+uint32(5*Word), 0x003f)
+	cpu.regs.PC = addr
+
+	for i := 0; i < 3; i++ {
+		if err := cpu.Step(); err != nil {
+			t.Fatalf("step %d failed: %v", i, err)
+		}
+	}
+
+	expected := uint16(0x002d)
+	if cpu.regs.SR != expected {
+		t.Fatalf("unexpected CCR value: got %04x want %04x", cpu.regs.SR, expected)
+	}
+
+	cpu.regs.PC = 0x3000
+	cpu.regs.SR = 0
+	ram.Write(Long, XPrivViolation<<2, 0x4000)
+
+	privileged := assemble(t, "ORI #$700,SR")
+	for i, b := range privileged {
+		ram.Write(Byte, cpu.regs.PC+uint32(i), uint32(b))
+	}
+
+	if err := cpu.Step(); err != nil {
+		t.Fatalf("privileged ORI failed: %v", err)
+	}
+	if cpu.regs.PC != 0x4000 {
+		t.Fatalf("privilege violation should vector to handler, PC=%04x", cpu.regs.PC)
+	}
+}
+
+func TestMoveToSrAndCcr(t *testing.T) {
+	cpu, ram := newEnvironment(t)
+
+	cpu.regs.SR = srSupervisor
+	originalSSP := cpu.regs.A[7]
+	cpu.regs.SSP = originalSSP
+	cpu.regs.USP = 0x5000
+
+	addr := cpu.regs.PC
+	ram.Write(Word, addr, 0x46fc) // MOVE #<data>,SR
+	ram.Write(Word, addr+uint32(Word), 0x0000)
+	ram.Write(Word, addr+uint32(2*Word), 0x44fc) // MOVE #<data>,CCR
+	ram.Write(Word, addr+uint32(3*Word), 0x000f)
+	cpu.regs.PC = addr
+
+	if err := cpu.Step(); err != nil {
+		t.Fatalf("MOVE to SR failed: %v", err)
+	}
+	if cpu.regs.SR != 0x0000 {
+		t.Fatalf("MOVE to SR should clear SR, got %04x", cpu.regs.SR)
+	}
+	if cpu.regs.A[7] != cpu.regs.USP {
+		t.Fatalf("MOVE to SR should switch to USP, SP=%04x USP=%04x", cpu.regs.A[7], cpu.regs.USP)
+	}
+	if cpu.regs.SSP != originalSSP {
+		t.Fatalf("MOVE to SR should preserve SSP, got %04x want %04x", cpu.regs.SSP, originalSSP)
+	}
+
+	if err := cpu.Step(); err != nil {
+		t.Fatalf("MOVE to CCR failed: %v", err)
+	}
+	expected := uint16(0x000f)
+	if cpu.regs.SR != expected {
+		t.Fatalf("MOVE to CCR should update low byte only, got %04x want %04x", cpu.regs.SR, expected)
+	}
+}
+
+func TestMoveFromSr(t *testing.T) {
+	cpu, ram := newEnvironment(t)
+
+	cpu.regs.SR = 0xa7f0
+	code := assemble(t, "MOVE SR,D2")
+	for i, b := range code {
+		ram.Write(Byte, cpu.regs.PC+uint32(i), uint32(b))
+	}
+
+	if err := cpu.Step(); err != nil {
+		t.Fatalf("MOVE SR,D2 failed: %v", err)
+	}
+	if cpu.regs.D[2]&0xffff != 0xa7f0 {
+		t.Fatalf("MOVE SR,D2 should copy SR, got %08x", cpu.regs.D[2])
+	}
+}
+
+func TestRteRestoresExceptionFrame(t *testing.T) {
+	cpu, ram := newEnvironment(t)
+
+	cpu.regs.SR = srSupervisor | 0x0700
+	cpu.regs.SSP = cpu.regs.A[7]
+	cpu.regs.USP = 0x6000
+	stackBase := uint32(0x3000)
+	cpu.regs.A[7] = stackBase
+
+	returnSR := uint16(0x0000)
+	returnPC := uint32(0x002000)
+	vectorOffset := uint32(0x0010)
+
+	ram.Write(Word, stackBase, uint32(returnSR))
+	ram.Write(Long, stackBase+uint32(Word), returnPC)
+	ram.Write(Word, stackBase+uint32(Word+Long), vectorOffset)
+
+	code := assemble(t, "RTE")
+	for i, b := range code {
+		ram.Write(Byte, cpu.regs.PC+uint32(i), uint32(b))
+	}
+
+	if err := cpu.Step(); err != nil {
+		t.Fatalf("RTE failed: %v", err)
+	}
+	if cpu.regs.PC != returnPC {
+		t.Fatalf("RTE should restore PC, got %08x want %08x", cpu.regs.PC, returnPC)
+	}
+	if cpu.regs.SR != returnSR {
+		t.Fatalf("RTE should restore SR, got %04x want %04x", cpu.regs.SR, returnSR)
+	}
+	if cpu.regs.SSP != stackBase+exceptionFrameSize {
+		t.Fatalf("RTE should advance SSP, got %04x want %04x", cpu.regs.SSP, stackBase+exceptionFrameSize)
+	}
+	if cpu.regs.A[7] != cpu.regs.USP {
+		t.Fatalf("RTE should switch to USP after clearing S bit, SP=%04x USP=%04x", cpu.regs.A[7], cpu.regs.USP)
+	}
+}
